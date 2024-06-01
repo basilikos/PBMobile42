30301ae7c8644a65f28cf35626953bab4f2588b3
basilikos <7a6f6b@gmail.com>
2024-06-02 00:42:51 +0300

    homework_01062024

--- src/main/java/config/AppiumConfig.java
+++ src/main/java/config/AppiumConfig.java
@@ -1,5 +1,7 @@
 package config;
 
+import helpers.PropertiesReaderXML;
+import interfaces.TestHelper;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -11,7 +13,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-public class AppiumConfig {
+public class AppiumConfig implements TestHelper {
 
     public static AppiumDriver<MobileElement> driver;
 
@@ -19,14 +21,21 @@ public class AppiumConfig {
     public void setUp() throws MalformedURLException {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "mobqa42");
-        capabilities.setCapability("platformVersion", "8.0");
-        capabilities.setCapability("appPackage", "com.sheygam.contactapp");
-        capabilities.setCapability("appActivity", ".SplashActivity");
+//        capabilities.setCapability("platformName", "Android");
+//        capabilities.setCapability("deviceName", "mobqa42");
+//        capabilities.setCapability("platformVersion", "8.0");
+//        capabilities.setCapability("appPackage", "com.sheygam.contactapp");
+//        capabilities.setCapability("appActivity", ".SplashActivity");
+//        capabilities.setCapability(MobileCapabilityType.APPLICATION_NAME, "Appium");
+//        capabilities.setCapability(MobileCapabilityType.APP, "/Users/zok/Documents/_TelRan/QA-42_Testing_Auto_43_29052024/contacts-android.apk");
+        capabilities.setCapability(PLATFORMNAME, PropertiesReaderXML.getProperty("platform", XML_FILE_PATH));
+        capabilities.setCapability(DEVICENAME, PropertiesReaderXML.getProperty("device", XML_FILE_PATH));
+        capabilities.setCapability(PLATFORMVERSION, PropertiesReaderXML.getProperty("version", XML_FILE_PATH));
+        capabilities.setCapability(PACKAGE, PropertiesReaderXML.getProperty("package", XML_FILE_PATH));
+        capabilities.setCapability(ACTIVITY, PropertiesReaderXML.getProperty("activity", XML_FILE_PATH));
 
-        capabilities.setCapability(MobileCapabilityType.APPLICATION_NAME, "Appium");
-        capabilities.setCapability(MobileCapabilityType.APP, "/Users/zok/Documents/_TelRan/QA-42_Testing_Auto_43_29052024/contacts-android.apk");
+        capabilities.setCapability(MobileCapabilityType.APPLICATION_NAME, PropertiesReaderXML.getProperty("appium", XML_FILE_PATH));
+        capabilities.setCapability(MobileCapabilityType.APP, PropertiesReaderXML.getProperty("appiumapp", XML_FILE_PATH));
 
         driver = new AppiumDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
---
+++ src/main/java/interfaces/TestHelper.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+public interface TestHelper {
+
+    public static final String XML_FILE_PATH = "src/main/resources/data.xml";
+
+    public static final String PLATFORMNAME = "platformName";
+    public static final String DEVICENAME = "deviceName";
+    public static final String PLATFORMVERSION = "platformVersion";
+    public static final String PACKAGE = "appPackage";
+    public static final String ACTIVITY = "appActivity";
+
+
+}
